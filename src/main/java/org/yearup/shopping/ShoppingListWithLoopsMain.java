8a7687d25a77e76cc5611bade9f4e42d58a159e8
Arey0904 <arey@appdev.yearup.org>
2023-04-20 16:21:56 -0400

    made changes

--- src/main/java/org/yearup/shopping/ShoppingListWithLoopsMain.java
+++ src/main/java/org/yearup/shopping/ShoppingListWithLoopsMain.java
@@ -1,39 +1,55 @@
 package org.yearup.shopping;
 
+import java.util.Scanner;
+
 public class ShoppingListWithLoopsMain {
+
+
     public static void main(String[] args) {
+        Scanner consoleInput = new Scanner(System.in);
 
+        while (true) {
 
-        // Show the user a list of items for sale
-        // Only show the product names and IDs, not the prices
+            System.out.println("Welcome to the store shop till your hearts content.");
+            // Show the user a list of items for sale
+            // Only show the product names and IDs, not the prices
+            Item[] itemsForSale = ItemsForSale.itemsForSale;
+            for (Item i : itemsForSale) {
+                if (!i.isInStock()) continue;
+                // System.out.println(i);
+                System.out.println(i.getDescription() + " " + i.getDescription());
 
-
-        Item[] itemsForSale = ItemsForSale.itemsForSale;
-        for (Item i : itemsForSale){
-            if ( ! i.isInStock()) continue;
-            // System.out.println(i);
-            System.out.println(i.getDescription());
-
-        }
-
-        // Invite the user to buy something
-
-        // Read in the user's response
-
-        // Parse their choice and get that item from the list
-
-        String selectedProductId = null;
-        Item selectedItem = null;
-
-        for (Item currentItemInListInTheLoop : itemsForSale ){
-            if (selectedProductId.equals(currentItemInListInTheLoop.getProductID())){
-                selectedItem = currentItemInListInTheLoop;
-                break;
             }
+            // Invite the user to buy something
+            System.out.println("Choose a product to buy (provide first 4 digits): ");
+            // Read in the user's response
+            String selectedProductId = consoleInput.nextLine();
+            // Parse their choice and get that item from the list
+
+            Item selectedItem = null;
+
+            for (Item currentItemInListInTheLoop : itemsForSale) {
+                if (selectedProductId.equals(currentItemInListInTheLoop.getProductID())) {
+                    selectedItem = currentItemInListInTheLoop;
+                    done = true;
+                    break;
+                }
+            }while (! done);
+            System.out.println("The price of this item is: " + " $" + selectedItem.getPrice());
+            System.out.println("Do you accept this price? (y/n)");
+            String YesOrNo = consoleInput.nextLine();
+
+            if (YesOrNo.equals("y")) {
+                System.out.println("Thank you enjoy your day.");
+                break;
+            } else if (YesOrNo.equals("n")) ;
+            {
+                System.out.println("Take a look at what else we got in store.");
+
+            }
+
+
         }
-
-
-
         // Tell the user they can have it for a price!
         // show them the price and ask them to accept it
 
